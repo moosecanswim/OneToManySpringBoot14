c74783319b4ff9f832ffaa48f901423696924fbf
moosecanswim <kyle.gallagher.09@gmail.com>
2017-08-29 08:26:04 -0400

    add movie to director does not work

--- src/main/java/me/moosecanswim/onetomany/Controller/MainController.java
+++ src/main/java/me/moosecanswim/onetomany/Controller/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -98,5 +99,23 @@ public class MainController {
         model.addAttribute("listMovies",movieRepository.findAll());
         return "showMovies";
     }
+    @RequestMapping("/assignMoviesToDirector/{id}")
+    public String directorDetails(Model model,@PathVariable("id") long id){
+        model.addAttribute("aDirector",directorRepository.findOne(id));
+        model.addAttribute("listMovies",movieRepository.findAll());
+        return "assignMoviesToDirector";
+    }
+    @RequestMapping("/processMovieAsigment/{directorId}/{movieId}")
+    public String processMovieAsigment(@PathVariable("movieId") long movieId,
+                                       @PathVariable("directorId") long directorId,
+                                       Model model){
+        Director aDirector = directorRepository.findOne(directorId);
+        Movie aMovie= movieRepository.findOne(movieId);
 
+        Set<Movie> temp=aDirector.getMovies();
+        temp.add(aMovie);
+
+        aDirector.setMovies(temp);
+        return "redirect:/showDirectors";
+    }
 }
